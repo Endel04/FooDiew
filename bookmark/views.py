a7d6cf4dbea34df2b970d918c8e0e191c65a48a9
Halam Yu <w2037@e-mirim.hs.kr>
2022-04-11 14:36:33 +0900

    views BookmarkListView

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from bookmark.models import Bookmark
 
-# Create your views here.
+
+class BookmarkListView(ListView):
+    model = Bookmark
+    # bookmark_list.html, {'bookmark_list' : Bookmark.objects.all()}
