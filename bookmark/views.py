e695710643a20f3e16375a42ef9c7b17a79d6d6b
Halam Yu <w2037@e-mirim.hs.kr>
2022-04-22 14:01:34 +0900

    get_absolute_url() in Bookmarki=

--- bookmark/views.py
+++ bookmark/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from bookmark.models import Bookmark
 
+
 class BookmarkListView(ListView):
     model = Bookmark
     #bookmark_list.html, {'bookmark_list': Bookmark.objects.all()}
@@ -15,14 +16,18 @@ class BookmarkCreateView(CreateView):
     template_name_suffix = '_create'    #bookmark_from.html -> bookmark_create.html
     success_url = reverse_lazy('bookmark:list')
 
-class BookmarkDetailView(DetailView):
-    model = Bookmark
 
 class BookmarkDetailView(DetailView):
     model = Bookmark
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
 
 class BookmarkUpdateView(UpdateView):
     model = Bookmark
     fields = ['name', 'url']  # '__all__'
     template_name_suffix = '_update'  # bookmark_update.html
-    success_url = reverse_lazy('bookmark:list')
+    # success_url = reverse_lazy('bookmark:list')
+    # success_url이 없으면 model의 get_absolute_url() 호출
